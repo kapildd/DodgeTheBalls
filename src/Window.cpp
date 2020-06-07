584434b198a98cd0237732a73bc39a87549fe623
Kapil Dolas <kapileyes@gmail.com>
2020-06-08 00:03:37 +0530

    Fixed constant names

--- src/Window.cpp
+++ src/Window.cpp
@@ -22,15 +22,15 @@ void Window::Init() {
   glEnable(GL_COLOR_MATERIAL);
   glEnable(GL_LIGHTING);
 
-  glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
-  glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
-  glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-  glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+  glLightfv(GL_LIGHT0, GL_AMBIENT, LIGHT_AMBIENT);
+  glLightfv(GL_LIGHT0, GL_DIFFUSE, LIGHT_DIFFUSE);
+  glLightfv(GL_LIGHT0, GL_SPECULAR, LIGHT_SPECULAR);
+  glLightfv(GL_LIGHT0, GL_POSITION, LIGHT_POSITION);
 
-  glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
-  glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
-  glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
-  glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
+  glMaterialfv(GL_FRONT, GL_AMBIENT, MAT_AMBIENT);
+  glMaterialfv(GL_FRONT, GL_DIFFUSE, MAT_DIFFUSE);
+  glMaterialfv(GL_FRONT, GL_SPECULAR, MAT_SPECULAR);
+  glMaterialfv(GL_FRONT, GL_SHININESS, HIGH_SHININESS);
 }
 
 GLFWwindow *Window::getWindow() { return window; }
